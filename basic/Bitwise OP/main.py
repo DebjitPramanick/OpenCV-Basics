1749dc67eed00e71739a8db6883ac223270ed6c2
DebjitPramanick <dpramanick9990@gmail.com>
2021-07-17 06:01:59 +0530

    Bitwise OP

---
+++ basic/Bitwise OP/main.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img1 = np.zeros((250, 500, 3), np.uint8)
+img1 = cv2.rectangle(img1, (200,0), (300, 100), (255,255,255), -1)
+img2 = cv2.imread('data/image_1.jpg')
+img1 = cv2.resize(img1, (512, 512))
+img2 = cv2.resize(img2, (512, 512))
+
+bitAnd = cv2.bitwise_and(img2, img1)
+bitOr = cv2.bitwise_or(img2, img1)
+bitNot = cv2.bitwise_not(img2, img1)
+bitXor = cv2.bitwise_xor(img2, img1)
+
+cv2.imshow('Image1', img1)
+cv2.imshow('Image2', img2)
+cv2.imshow('BitAND2', bitAnd)
+# cv2.imshow('BitOR2', bitOr)
+# cv2.imshow('BitNOT2', bitNot)
+# cv2.imshow('BitXOR2', bitXor)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
