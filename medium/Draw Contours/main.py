e9ff6c5886de5ffa756bfab798a1aac622ba21a0
DebjitPramanick <dpramanick9990@gmail.com>
2021-08-10 17:33:36 +0530

    Find and Draw Contours

---
+++ medium/Draw Contours/main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('data/opencv-logo.png')
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Find the contours
+ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+# Draw teh contours
+
+cv2.drawContours(img, contours, -1, (186,202,86), 6)
+
+cv2.imshow('Image', img)
+cv2.imshow('Image GRAY', imgray)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
